a3a984099d9f22b59ad195d329d21649acef0b4d
Michael Farquhar <michaelfarquhar1991@gmail.com>
2021-12-02 00:50:32 +0000

    Move challenge logic into own function

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -6,29 +6,31 @@ fn get_numbers() -> Vec<i32> {
         .collect()
 }
 
-fn puzzle1(numbers: &[i32]) {
-    let mut deeper_count = 0;
-    for i in 0..numbers.len() - 1 {
-        let prev_number = numbers[i];
-        let number = numbers[i + 1];
-        if number > prev_number {
-            deeper_count += 1;
+/// Counts the windows that increase in total value when compared to the
+/// previous window.
+/// 
+/// Windows are contiguous, fixed size slices of the array that overlap starting
+/// at the beginning and increasing in their starting index by 1 until no more
+/// full windows can be created.
+fn count_increases_windowed(numbers: &[i32], window_size: usize) -> i32 {
+    let mut count = 0;
+    for i in 0..numbers.len() - window_size {
+        let total_a: i32 = numbers[i..(i + window_size)].iter().sum();
+        let total_b: i32 = numbers[(i + 1)..=(i + window_size)].iter().sum();
+        if total_b > total_a {
+            count += 1;
         }
     }
+    count
+}
 
+fn puzzle1(numbers: &[i32]) {
+    let deeper_count = count_increases_windowed(numbers, 1);
     println!("Puzzle 1: Depth increases: {}", deeper_count);
 }
 
 fn puzzle2(numbers: &[i32]) {
-    let mut deeper_count = 0;
-    for i in 0..numbers.len() - 3 {
-        let total_a: i32 = numbers[i..i+3].iter().sum();
-        let total_b: i32 = numbers[i+1..i+4].iter().sum();
-        if total_b > total_a {
-            deeper_count += 1;
-        }
-    }
-
+    let deeper_count = count_increases_windowed(numbers, 3);
     println!("Puzzle 2: Depth increases: {}", deeper_count);
 }
 
