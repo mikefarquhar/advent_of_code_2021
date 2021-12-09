4490d00c2d561605d21812471e87391eb43c9ebc
Michael Farquhar <michaelfarquhar1991@gmail.com>
2021-12-09 23:23:47 +0000

    Reduce work done

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -10,75 +10,79 @@ fn get_data() -> Vec<Vec<u32>> {
         .collect()
 }
 
-fn part1(terrain: &Vec<Vec<u32>>) -> u32 {
-    let mut low_points = Vec::new();
+fn get_basin_minima(terrain: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
+    let mut minima = Vec::new();
 
     for y in 0..terrain.len() {
         for x in 0..terrain[y].len() {
-            let current = terrain[y][x];
+            let height = terrain[y][x];
 
-            if (y > 0 && current >= terrain[y - 1][x])
-                || (y < terrain.len() - 1 && current >= terrain[y + 1][x])
-                || (x > 0 && current >= terrain[y][x - 1])
-                || (x < terrain[y].len() - 1 && current >= terrain[y][x + 1])
+            if (y > 0 && height >= terrain[y - 1][x])
+                || (y < terrain.len() - 1 && height >= terrain[y + 1][x])
+                || (x > 0 && height >= terrain[y][x - 1])
+                || (x < terrain[y].len() - 1 && height >= terrain[y][x + 1])
             {
                 continue;
             }
 
-            low_points.push(current + 1);
+            minima.push((x, y));
         }
     }
 
-    low_points.iter().sum()
+    minima
 }
 
-fn part2(terrain: &Vec<Vec<u32>>) -> u32 {
-    let mut terrain: Vec<Vec<u32>> = terrain.iter().map(|row| row.clone()).collect();
+fn part1(terrain: &Vec<Vec<u32>>, basin_minima: &Vec<(usize, usize)>) -> u32 {
+    basin_minima
+        .iter()
+        .fold(0, |acc, (x, y)| acc + terrain[*y][*x] + 1)
+}
 
-    let mut to_process = Vec::new();
+fn part2(terrain: &mut Vec<Vec<u32>>, basin_minima: &Vec<(usize, usize)>) -> u32 {
+    let mut floodfill_stack = Vec::new();
     let mut basin_sizes = Vec::new();
 
-    for y in 0..terrain.len() {
-        for x in 0..terrain[0].len() {
-            if terrain[y][x] == 9 {
-                continue;
-            }
+    for position in basin_minima {
+        let (x, y) = *position;
 
-            to_process.push((x, y));
-            terrain[y][x] = 9;
-
-            let mut basin_size = 1;
-
-            while to_process.len() > 0 {
-                let (x, y) = to_process.pop().unwrap();
-
-                if y > 0 && terrain[y - 1][x] < 9 {
-                    to_process.push((x, y - 1));
-                    terrain[y - 1][x] = 9;
-                    basin_size += 1;
-                }
-
-                if y < terrain.len() - 1 && terrain[y + 1][x] < 9 {
-                    to_process.push((x, y + 1));
-                    terrain[y + 1][x] = 9;
-                    basin_size += 1;
-                }
-
-                if x > 0 && terrain[y][x - 1] < 9 {
-                    to_process.push((x - 1, y));
-                    terrain[y][x - 1] = 9;
-                    basin_size += 1;
-                }
-
-                if x < terrain[y].len() - 1 && terrain[y][x + 1] < 9 {
-                    to_process.push((x + 1, y));
-                    terrain[y][x + 1] = 9;
-                    basin_size += 1;
-                }
-            }
-
-            basin_sizes.push(basin_size);
+        if terrain[y][x] == 9 {
+            continue;
         }
+
+        floodfill_stack.push((x, y));
+        terrain[y][x] = 9;
+
+        let mut basin_size = 1;
+
+        while floodfill_stack.len() > 0 {
+            let (x, y) = floodfill_stack.pop().unwrap();
+
+            if y > 0 && terrain[y - 1][x] < 9 {
+                floodfill_stack.push((x, y - 1));
+                terrain[y - 1][x] = 9;
+                basin_size += 1;
+            }
+
+            if y < terrain.len() - 1 && terrain[y + 1][x] < 9 {
+                floodfill_stack.push((x, y + 1));
+                terrain[y + 1][x] = 9;
+                basin_size += 1;
+            }
+
+            if x > 0 && terrain[y][x - 1] < 9 {
+                floodfill_stack.push((x - 1, y));
+                terrain[y][x - 1] = 9;
+                basin_size += 1;
+            }
+
+            if x < terrain[y].len() - 1 && terrain[y][x + 1] < 9 {
+                floodfill_stack.push((x + 1, y));
+                terrain[y][x + 1] = 9;
+                basin_size += 1;
+            }
+        }
+
+        basin_sizes.push(basin_size);
     }
 
     basin_sizes.sort_by(|a, b| b.partial_cmp(a).unwrap());
@@ -86,7 +90,8 @@ fn part2(terrain: &Vec<Vec<u32>>) -> u32 {
 }
 
 fn main() {
-    let data = get_data();
-    println!("Part 1: {}", part1(&data));
-    println!("Part 2: {}", part2(&data));
+    let mut terrain = get_data();
+    let basin_minima = get_basin_minima(&terrain);
+    println!("Part 1: {}", part1(&terrain, &basin_minima));
+    println!("Part 2: {}", part2(&mut terrain, &basin_minima));
 }
