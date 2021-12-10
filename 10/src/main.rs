4e342f4c0c45c2e8bebe2690611bd84dfdde6532
Michael Farquhar <michaelfarquhar1991@gmail.com>
2021-12-10 16:41:34 +0000

    Day 10

---
+++ 10/src/main.rs
@@ -0,0 +1,75 @@
+fn get_data() -> Vec<&'static str> {
+    let data = include_str!("./input.txt");
+    data.lines().collect()
+}
+
+fn score_lines(lines: &Vec<&str>) -> (u64, u64) {
+    let mut error_score = 0;
+    let mut autocomplete_scores = Vec::new();
+    let mut brace_stack = Vec::new();
+
+    for line in lines {
+        let start_error = error_score;
+
+        for char in line.chars() {
+            match char {
+                '(' | '[' | '{' | '<' => brace_stack.push(char),
+                ')' => {
+                    if brace_stack.pop().unwrap() != '(' {
+                        error_score += 3;
+                        break;
+                    }
+                },
+                ']' => {
+                    if brace_stack.pop().unwrap() != '[' {
+                        error_score += 57;
+                        break;
+                    }
+                },
+                '}' => {
+                    if brace_stack.pop().unwrap() != '{' {
+                        error_score += 1197;
+                        break;
+                    }
+                },
+                '>' => {
+                    if brace_stack.pop().unwrap() != '<' {
+                        error_score += 25137;
+                        break;
+                    }
+                },
+                _ => (),
+            };
+        }
+
+        if error_score == start_error {
+            let autocomplete_score = brace_stack
+                .iter()
+                .rev()
+                .fold(0, |acc, char| {
+                    acc * 5 + match char {
+                        '(' => 1,
+                        '[' => 2,
+                        '{' => 3,
+                        '<' => 4,
+                        _ => 0,
+                    }
+                });
+            autocomplete_scores.push(autocomplete_score);
+        }
+
+        brace_stack.clear();
+    }
+
+    autocomplete_scores.sort();
+    let autocomplete_score = autocomplete_scores[autocomplete_scores.len() / 2];
+
+    (error_score, autocomplete_score)
+}
+
+fn main() {
+    let lines = get_data();
+    let (error_score, autocomplete_score) = score_lines(&lines);
+    println!("Syntax error score: {}", error_score);
+    println!("Autocomplete score: {}", autocomplete_score);
+}
